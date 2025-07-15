32491dd341847e66a76c7bd11d43bf74993aee51
KEWIN ANDRES BECERRA FERNANDEZ <kewinbecerra1113588@correo.itm.edu.co>
2025-07-15 16:45:34 -0500

    Se agrego el localstorage

--- API_RandMPersonajes/Program.cs
+++ API_RandMPersonajes/Program.cs
@@ -1,10 +1,12 @@
 using API_RandMPersonajes;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazoredLocalStorage();
 
 var urlLocal = "https://localhost:7137";
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(urlLocal) });
